625ae22b07da3276f83fe6c550f554e501d6e12f
Raghavendra Dasari <162073459+raghu0209@users.noreply.github.com>
2026-04-10 19:03:03 +0530

    Add files via upload

---
+++ src/main/java/com/lms/auth/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.lms.auth.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lms.auth.dto.AccountLoginDto;
+import com.lms.auth.dto.AccountRegisterDto;
+import com.lms.auth.service.AuthService;
+import com.lms.auth.utils.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+	
+	private final AuthService authService;
+	
+	@PostMapping("/register")
+	public ApiResponse register(@RequestBody AccountRegisterDto accountRegisterDto) {
+		return authService.register(accountRegisterDto);
+	}
+	
+	@PostMapping("/login")
+	public ApiResponse login(@RequestBody AccountLoginDto accountLoginDto) {
+		return authService.login(accountLoginDto);
+	}
+}
